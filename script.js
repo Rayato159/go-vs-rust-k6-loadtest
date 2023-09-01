af98b84be60c1ef05ecc03dc90398c06fcd045ac
Rayato159 <rayato159@gmail.com>
2023-09-01 14:03:19 +0700

    -feature :sparkles: update README.md

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '30s', target: 100 },
-    { duration: '30s', target: 1000 },
-    { duration: '30s', target: 10000 },
-  ],
+  vus: 10000,
+  duration: '30s',
 };
 
 export default function () {
-  const res = http.get('https://httpbin.test.k6.io/');
+  const res = http.get('http://0.0.0.0:3000/');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
